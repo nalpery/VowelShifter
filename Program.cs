4d56278c98ec5f893beca7d8b726e6b10011ca06
nalpery <nalpery@gmail.com>
2022-08-26 06:25:35 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Bir metin giriniz:");
+string str = Console.ReadLine();
+string strnew = string.Empty;
+List<char> vowel = new List<char> { };   
+char[] consonant = new char[str.Length];
+
+for (int i = 0; i < str.Length; i++)
+{
+    if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u') 
+    { 
+        vowel.Add(str[i]);
+        consonant[i] = '0';
+        
+    }
+    else 
+    { 
+        consonant[i]=str[i]; 
+        
+    }
+    
+}int counter = 0;
+for (int i = 0; i < str.Length; i++)
+{
+    if (consonant[i] != '0') { strnew += consonant[i].ToString(); }
+    else
+    {
+        
+        strnew += vowel[(vowel.Count-1+counter)%vowel.Count].ToString();
+        counter++;
+
+    }
+
+}
+
+Console.WriteLine($"{strnew}");
